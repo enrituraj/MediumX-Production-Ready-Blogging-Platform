5693754a98776170eeda39651fb987849e389995
Ritu Raj <rn26rituraj@gmail.com>
2026-06-13 07:12:17 +0530

    creating the hero page

--- client/src/pages/public/Homepage.tsx
+++ client/src/pages/public/Homepage.tsx
@@ -3,16 +3,26 @@ import { Link } from 'react-router-dom'
 
 const Homepage = () => {
   return (
-    <div className='flex justify-between items-center h-screen bg-amber-50'>
-      <div className='container mx-auto'>
-        <h1 className='text-9xl'>Human <br />
-          stories & ideas</h1>
-        <p>A place to read, write, and deepen your understanding</p>
-        <Link to="/login">Start Reading</Link>
-      </div>
+    <div className='bg-amber-50'>
+      <div className='max-w-7xl mx-auto'>
+        <div className='flex justify-between items-center min-h-[calc(100vh-80px-50px)]'>
+          <div className='flex flex-col gap-8'>
+
+            <h1 className='text-9xl font-semibold'>Human <br />
+              stories & ideas</h1>
+            <p className='text-2xl'>A place to read, write, and deepen your understanding</p>
+            <Link to="/login" className='w-fit px-8 py-2 bg-black text-2xl text-white rounded-full'>Start reading</Link>
+          </div>
+
         <div>
-          image
+          <img src="https://miro.medium.com/v2/format:webp/4*SdjkdS98aKH76I8eD0_qjw.png" 
+           alt="" 
+          className='w-125 absolute right-0 top-50'
+           />
         </div>
+
+        </div>
+      </div>
     </div>
   )
 }
--- client/src/pages/public/PublicLayouts.tsx
+++ client/src/pages/public/PublicLayouts.tsx
@@ -3,46 +3,42 @@ import { Link, Outlet } from 'react-router-dom'
 const PublicLayouts = () => {
   return (
     <>
-    <div className=" m-auto bg-amber-100">
-
-        <nav className='max-w-7xl m-auto flex justify-between items-center p-4'>
-            <Link to="/">
-            
-            <h1>MediumX</h1>
-            </Link>
-            <div className='flex gap-4'>
-
-
+    <div className=''>
+      <div className=" m-auto bg-amber-50 border border-b-2">
+        <nav className='max-w-7xl m-auto flex justify-between items-center p-4 '>
+          <Link to="/">
+            <h1 className='text-3xl font-bold'>MediumX</h1>
+          </Link>
+          <div className='flex gap-4 items-center'>
             <Link to="/about">Our Story</Link>
             <Link to="/membership">Membership</Link>
-            <Link to="/write">write</Link>
-            <Link to="/signin">sign in</Link>
-            <Link to="/signup">get started</Link>
-            </div>
+            <Link to="/write">Write</Link>
+            <Link to="/signin">Sign in</Link>
+            <Link to="/signup" className='px-4 py-2 bg-black text-white rounded-full'>Get started</Link>
+          </div>
         </nav>
+      </div>
+      <main className='min-h-[calc(100vh-80px-50px)]'>
+        <Outlet />
+      </main>
+      <footer className='p-4 bg-amber-50 flex justify-center items-center gap-4 border border-t-2'>
+
+        <div className='flex gap-4 text-xs'>
+          <Link to="#">Help</Link>
+          <Link to="#">Status</Link>
+          <Link to="#">About</Link>
+          <Link to="#">Careers</Link>
+          <Link to="#">Press</Link>
+          <Link to="#">Blog</Link>
+          <Link to="#">Store</Link>
+          <Link to="#">Privacy</Link>
+          <Link to="#">Rules</Link>
+          <Link to="#">Terms</Link>
+          <Link to="#">Text to speech</Link>
+
+        </div>
+      </footer>
     </div>
-        <main>
-            <Outlet/>
-        </main>
-        <footer className='p-4 bg-amber-50 flex justify-center items-center gap-4 mt-4'>
-            
-            <div className='flex gap-4 text-xs'>
-              <Link to="#">Help</Link>
-              <Link to="#">Status</Link>
-              <Link to="#">About</Link>
-              <Link to="#">Careers</Link>
-              <Link to="#">Press</Link>
-              <Link to="#">Blog</Link>
-              <Link to="#">Store</Link>
-              <Link to="#">Privacy</Link>
-              <Link to="#">Rules</Link>
-              <Link to="#">Terms</Link>
-              <Link to="#">Text to speech</Link>
-
-            </div>
-
-
-        </footer>
     </>
   )
 }
